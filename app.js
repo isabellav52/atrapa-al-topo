1968808d7484e6cb0dd9b5af2c19ca18dfc372d9
isabellav52 <123976477+isabellav52@users.noreply.github.com>
2023-05-15 11:34:55 -0500

    a

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempofaltante = document.querySelector("#tiempo");
+let puntaje = document.getElementById("puntaje");
+
+let resultado = 0;
+let tiempoActual = tiempofaltante.textContent;
+

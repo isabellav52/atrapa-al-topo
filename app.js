aca7c3a88882fc9a8029108272e28173fdd5e4bc
isabellav52 <123976477+isabellav52@users.noreply.github.com>
2023-06-05 11:34:56 -0500

    topo

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
-let posiciontopo = null
+let posiciontopo = null;
+
 
 function  cuadradoazar () {
  cuadrado.forEach ((nombredeclase => {
@@ -16,3 +17,35 @@ let posicionalazar = cuadrado[Math.floor (Math.random()*9)];
 posicionalazar.classList.add("topo");
 
 posiciontopo = posicionalazar.id;
+}
+
+cuadro.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if(identificador.id === posiciontopo){
+      resultado = resultado + 1;
+      puntaje.textContent = resultado;
+      posicionalazar = null;
+    }
+  });
+});
+
+function movertopo(){
+  tiempotopo = setInterval(cuadroazar, 700);
+}
+movertopo();
+
+function cuentaregresiva() {
+  tiempoactual--;
+  tiempofaltante.textContent = tiempoactual;
+  if (tiempoactual === 0) {
+    clearInterval(idtiempo);
+    clearInterval(tiempotopo);
+    alert(
+      "se acabo el tiempo, tu puntaje es" + resultado + "topos atrapados"
+
+    );
+  }
+
+}
+
+let idtiempo = setInterval(cuentaregresiva, 1000);
